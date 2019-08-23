9eb0cddfd85aceb121a2bc706ac086de6e42ece9
Joey Comhaire <joey.comhaire@gmail.com>
2019-08-23 15:44:38 +0200

    rename

--- rename.js
+++ rename.js
@@ -4,6 +4,10 @@ const slides = fs.readdirSync('./src/slides').filter(slide => slide.indexOf('pla
 
 slides.forEach(slide => {
   const nameParts = slide.split('-');
-  const num = nameParts[0] === ''
-  fs.renameSync(slide, )
+  const num = nameParts[0].startsWith('0') ? nameParts[0].substring(1) : nameParts[0];
+  const newNum = parseInt(num) + 1;
+  const newNumPart = newNum < 10 ? '0' + newNum : '' + newNum;
+  const oldPath = `./src/slides/${slide}`;
+  const newPath = `./src/slides/${newNumPart}-${nameParts[1]}`;
+  fs.renameSync(oldPath, newPath);
 });
