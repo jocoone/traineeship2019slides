f0e4ebc865c5d7cd0511a95b65f3c42f96873e99
Joey Comhaire <joey.comhaire@gmail.com>
2019-09-01 18:04:29 +0200

    rename

--- rename.js
+++ rename.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
+const _ = require('lodash');
+const args = require('yargs')
+  .string('start')
+  .argv;
 
 const slides = fs.readdirSync('./src/slides').filter(slide => slide.indexOf('placeholder.html') < 0);
+const filteredSlides = _.takeRight(slides, slides.length - slides.indexOf(args.start));
+const orderedSlides = _.orderBy(filteredSlides, ['asc']);
 
-slides.forEach(slide => {
+console.log(args.start);
+
+_.forEachRight(orderedSlides, slide => {
   const nameParts = slide.split('-');
   const num = nameParts[0].startsWith('0') ? nameParts[0].substring(1) : nameParts[0];
   const newNum = parseInt(num) + 1;
